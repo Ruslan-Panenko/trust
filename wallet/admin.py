d6f804c129dc4a91b8ca84c9ff9e9d71a6f7c29f
Ruslan Panenko <panenko.ruslan1@gmail.com>
2024-03-21 05:18:54 +0200

    admin

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from .models import Phrase
 
-from wallet.models import Phrase
 
-# Register your models here.
-admin.site.register(Phrase)
+class PhraseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'phrase', 'is_used']
+    list_filter = ['is_used']
+
+
+admin.site.register(Phrase, PhraseAdmin)
